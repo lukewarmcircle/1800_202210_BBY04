9661f903ee14007397eb18b0363705f16111b4ad
Caroline Lin <caroline.yclin@gmail.com>
2022-03-29 11:22:55 -0700

    .

--- scripts/eachRestaurant.js
+++ scripts/eachRestaurant.js
@@ -61,6 +61,8 @@ function read_description() {
                 console.log(description.data().name);
             })
     }
+
+    
 }
 
 read_description();
@@ -96,3 +98,19 @@ function showSlides(n) {
 
 
 }
+
+//
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.createElement('input');
+    button.type = 'button';
+    button.id = 'submit';
+    button.value = 'Submit';
+    button.className = 'btn';
+ 
+    button.onclick = function() {
+    // …
+    };
+ 
+    var container = document.getElementById('translation');
+    container.appendChild(button);
+}, false);
---
+++ scripts/translation.js
@@ -0,0 +1,12 @@
+// top bar tooltip (translation)
+$(document).ready(function() {
+    $('#translation').append(
+        $(document.createElement('button')).prop({
+            type: 'button',
+            innerHTML: 'Press me',
+            class: 'btn-styled'
+        })
+    );
+});
+
+//
